e65b128a7e92eb58b1acef963ced0d4384fc59cd
Chidozie <109086527+Chido100@users.noreply.github.com>
2023-10-25 14:13:28 +0100

    Updated users model

--- quick_care/users/models.py
+++ quick_care/users/models.py
@@ -3,18 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 
 
-USER_STATUS = (
-    ('unapproved', 'Unapproved'),
-    ('approved', 'Approved')
-)
-
-
-
 class User(AbstractUser):
     is_carer = models.BooleanField(default=False)
 
     email = models.EmailField(unique=True)
-    user_status = models.CharField(max_length=20, choices=USER_STATUS, default='unapproved')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
